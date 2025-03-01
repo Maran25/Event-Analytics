7eca62e83e5085af5085105affb3f9c3078c3ea3
Maran <119050451+Maran25@users.noreply.github.com>
2025-03-01 18:50:51 +0530

    timeout and max clients added to the pg pool

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,13 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT) || 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT) || 5432,
+  max: 20, 
+  idleTimeoutMillis: 30000,
 });
 
 pool.on("connect", () => {
